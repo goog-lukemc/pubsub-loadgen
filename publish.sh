3de9a8b9930acbcd867455c065e64ea80b24c364
Luke <lukemc@google.com>
2019-04-24 16:54:52 -0400

    publish build

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
-env GOOS=windows GOARCH=amd64 go build -o pubsub-loadgen_amd64.exe
-env GOOS=windows GOARCH=386 go build -o pubsub-loadgen_x86.exe
-env GOOS=linux GOARCH=amd64 go build -o pubsub-loadgen_amd64_linux
-env GOOS=linux GOARCH=386 go build -o pubsub-loadgen_x86_linux
-env GOOS=darwin GOARCH=amd64 go build -o pubsub-loadgen_amd64_mac
-env GOOS=darwin GOARCH=386 go build -o pubsub-loadgen_x86_mac
-git commit add .
+env GOOS=windows GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64.exe
+env GOOS=windows GOARCH=386 go build -o compiled/pubsub-loadgen_x86.exe
+env GOOS=linux GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64_linux
+env GOOS=linux GOARCH=386 go build -o compiled/pubsub-loadgen_x86_linux
+env GOOS=darwin GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64_mac
+env GOOS=darwin GOARCH=386 go build -o compiled/pubsub-loadgen_x86_mac
+git add .
 git commit -am "publish build"
-git push master
+git push origin master
 
 
 
