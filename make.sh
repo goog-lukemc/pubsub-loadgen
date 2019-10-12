eb34dcc7fa66ca8073a2a2c6105d27ebc0885c79
Luke McCoy <lukemc@google.com>
2019-10-12 09:06:17 -0400

    updated make

--- make.sh
+++ make.sh
@@ -1,6 +1,10 @@
+set -e
+
 git checkout master
 HASH=$(git rev-parse HEAD)
-git checkout -b "rebuild_$HASH"
+
+BRANCH="rebuild_$HASH"
+git checkout -b $BRANCH
 
 env GOOS=windows GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64.exe
 env GOOS=windows GOARCH=386 go build -o compiled/pubsub-loadgen_x86.exe
@@ -12,7 +16,6 @@ env GOOS=darwin GOARCH=386 go build -o compiled/pubsub-loadgen_x86_mac
 git add .
 
 git commit -am "Updated Compiled Source with master hash:$HASH"
-git push
-
-
-
+git checkout master
+git merge $BRANCH
+git push origin master
