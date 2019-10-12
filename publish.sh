8d99db67cbc157bce593f76768df77226b04c8f5
Luke McCoy <lukemc@google.com>
2019-10-12 08:28:45 -0400

    updated build script

--- publish.sh
+++ publish.sh
@@ -1,12 +1,18 @@
+git checkout master
+HASH=$(git rev-parse HEAD)
+git checkout -b "rebuild_$HASH"
+
 env GOOS=windows GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64.exe
 env GOOS=windows GOARCH=386 go build -o compiled/pubsub-loadgen_x86.exe
 env GOOS=linux GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64_linux
 env GOOS=linux GOARCH=386 go build -o compiled/pubsub-loadgen_x86_linux
 env GOOS=darwin GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64_mac
 env GOOS=darwin GOARCH=386 go build -o compiled/pubsub-loadgen_x86_mac
-git add .
-git commit -am "publish build"
 
+git add .
+
+git commit -am "Updated Compiled Source with master hash:$HASH"
+git push
 
 
 
