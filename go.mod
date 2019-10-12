f42fe2f51fb0aa591d712d6dacd3a2ad72570d9a
Luke McCoy <lukemc@google.com>
2019-10-12 08:20:14 -0400

    change build and add go mode

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/goog-lukemc/pubsub-loadgen
+
+go 1.13
+
+require cloud.google.com/go/pubsub v1.0.1
--- publish.sh
+++ publish.sh
@@ -6,7 +6,6 @@ env GOOS=darwin GOARCH=amd64 go build -o compiled/pubsub-loadgen_amd64_mac
 env GOOS=darwin GOARCH=386 go build -o compiled/pubsub-loadgen_x86_mac
 git add .
 git commit -am "publish build"
-git push origin master
 
 
 
